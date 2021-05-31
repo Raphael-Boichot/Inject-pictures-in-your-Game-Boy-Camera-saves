50ee60878c5b6f9c7ba2e624bbc861f61ac5bf83
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2021-05-31 17:17:09 +0200

    Add files via upload

---
+++ Research/Image_uneraser.m
@@ -0,0 +1,18 @@
+clc
+clear
+%------------------------------------------------------------------------
+fid = fopen('GAMEBOYCAMERA.sav','r');    %save file you want to unerase
+while ~feof(fid)
+a=fread(fid);
+end
+
+vector_state=[0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29];
+checksum=[226;20];
+fclose(fid);
+%------------------------------------------------------------------------
+a(4531:4560)=vector_state;
+a(4566:4567)=checksum;
+fid = fopen('Unerased_save.sav','w');
+a=fwrite(fid,a);
+fclose(fid);
+msgbox('End of injection, file ready to burn');
