da33d7e2aba72fb76b01a025e5564ea8f590d7cd
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2021-06-07 22:38:58 +0200

    Add files via upload

--- Codes/slot_viewer.m
+++ Codes/slot_viewer.m
@@ -1,4 +1,6 @@
-%By Raphaël BOICHOT, 1 june 2021
+%By Raphaël BOICHOT, 7 june 2021
+clc
+clear
 fid = fopen('GAMEBOYCAMERA.sav','r');    
 while ~feof(fid)
 a=fread(fid);
@@ -66,5 +68,34 @@ imagesc(decode(imagek))
 colormap(gray)
 end
 
+% 	0x010BB-0x010BC: counter for image taken (on 2x2 digits reversed);
+% 	0x010BD-0x010BE: counter for image erased (on 2x2 digits reversed);
+% 	0x010BF-0x010C0: counter for image transfered (on 2x2 digits reversed);
+% 	0x010C1-0x010C2: counter for image printed (on 2x2 digits reversed);
+% 	0x010C3-0x010C4: counter for pictures received by males an females (2x2 digits);
+% 	0x010C5-0x010C6: Score at Space Fever II (on 4x2 digits reversed);
+% 	0x010C9-0x010CA: score at balls (on 2x2 digits reversed);
+% 	0x010CB-0x010CC: score at Run! Run! Run! (on 2x2 digits reversed, 99 minus value on screen);
+% 	0x010D2-0x010D6: "Magic" word in ascii;
+% 	0x010D7-0x010D8: checksum (2 bytes, left is a 8-bit sum, rigth is a 8-bit XOR);
 
+minigames=a(4284:4313);
+disp(['number of image taken: ',dec2hex(minigames(2),2),dec2hex(minigames(1),2)]);
+disp(['number of image erased: ',dec2hex(minigames(4),2),dec2hex(minigames(3),2)]);
+disp(['number of image transfered: ',dec2hex(minigames(6),2),dec2hex(minigames(5),2)]);
+disp(['number of image exchanged with males: ',dec2hex(minigames(7),2)]);
+disp(['number of image exchanged with females: ',dec2hex(minigames(8),2)]);
+space_fever=[dec2hex(minigames(12),2),dec2hex(minigames(11),2),dec2hex(minigames(10),2),dec2hex(minigames(9),2)];
+disp(['Score at Space Fever II: ',space_fever]);
+disp(['Score at Ball: ',dec2hex(minigames(14),2),dec2hex(minigames(13),2)]);
+cent=[dec2hex(minigames(15),2)];
+d3=9-str2num(cent(1));
+d4=9-str2num(cent(2));
+cent=[num2str(d3),num2str(d4)];
+sec=[dec2hex(minigames(16),2)];
+d1=9-str2num(sec(1));
+d2=9-str2num(sec(2));
+sec=[num2str(d1),num2str(d2)];
+disp(['Time at Run!Run!Run!: ',sec,'.',cent,' sec']);
+disp(['Checksum: ', dec2hex(minigames(29),2),' ',dec2hex(minigames(30),2)])
 
